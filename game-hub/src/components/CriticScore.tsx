03aef05f35816835b610da98cb2a9ff377b1b92e
miss.mary <maryost24@gmail.com>
2024-07-19 21:56:56 +0330

    Change the name of CriticScore Components

--- game-hub/src/components/CriticScore.tsx
+++ game-hub/src/components/CriticScore.tsx
@@ -3,11 +3,11 @@ import {Badge} from "@chakra-ui/react";
 interface Props{
     score: number;
 }
-const MyComponent = ({score}: Props) => {
+const CriticScore = ({score}: Props) => {
     let color = score > 75 ? 'green' : score > 65 ? 'yellow' : '';
     return (
         <Badge colorScheme={color} fontSize='14px' paddingX={2} borderRadius='4px'>{score}</Badge>
     );
 };
 
-export default MyComponent;
+export default CriticScore;
