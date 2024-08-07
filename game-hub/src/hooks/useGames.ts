5013c115dd55a87889f84e7037e4a0f07c72d360
miss.mary <maryost24@gmail.com>
2024-08-07 10:56:47 +0330

    Refactoe- simplify time calculation

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -1,9 +1,10 @@
 
 import {GameQuery} from "../App";
-import {useInfiniteQuery, useQuery} from "@tanstack/react-query";
+import {useInfiniteQuery} from "@tanstack/react-query";
 import APIClient , {FetchResponse} from "../services/api-client";
 import {Platform} from "./usePlatforms";
-import {all} from "axios";
+
+import ms from "ms";
 
 const apiClient =  new APIClient<Game>('/games');
 
@@ -33,7 +34,7 @@ const useGames = (gameQuery: GameQuery) => {
         getNextPageParam : (lastPage, allPages) => {
             return lastPage.next ? allPages.length + 1 : undefined
         },
-        staleTime: 24 * 60 * 60 * 1000 //24h
+        staleTime: ms('24h'),
     })
 
 };
